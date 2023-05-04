94dbd812f93d49b06aaf2cccc38f0a526ec7a0ad
mrmiguetara <miguelag1999@hotmail.com>
2023-05-04 13:25:52 -0400

    Saving changes

--- dancing.py
+++ dancing.py
@@ -4,18 +4,22 @@ from time import time
 from ortools.linear_solver import pywraplp
 import argparse
 
+
+class PARS(object):
+    def __init__(self) -> None:
+        pass
+
+
 def model(c,c_prime, k, r, s, p, I, J, T):
     start_time = time()
 
     solver = pywraplp.Solver('LAP', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
     x = {}
     y = {}
-    h = {}
     for i in range(I):
         for j in range(J):
             x[i,j] = solver.BoolVar(name='x[%i,%i]' % (i,j))
             y[i,j] = solver.BoolVar(name='y[%i,%i]' % (i,j))
-            h[i,j] = solver.BoolVar(name='h[%i,%i]' % (i,j))
 
     solver.Maximize(solver.Sum([c[i][j]*x[i,j] + c_prime[i][j]*y[i, j] for i in range(I) for j in range(J)]))
 
@@ -38,31 +42,6 @@ def model(c,c_prime, k, r, s, p, I, J, T):
     for i in range(I):
         solver.Add(solver.Sum([y[i,j] for j in range(J)]) <= k[i]*y[i,i])
 
-    for i in range(I):
-        for j in range(J):
-            if i != j:
-                for t in range(T):
-                    if t < r[i]:
-                        solver.Add(p[j][t] * x[i,j] <= h[i,t])
-    for i in range(I):
-        for t in range(T):
-            if r[i] < t and t > s[i]:
-                solver.Add(p[i][t] * x[i,i] == h[i,t])
-    for i in range(I):
-        for j in range(J):
-            if i != j:
-                for t in range(T):
-                    if t > s[i]:
-                        solver.Add(p[j][t] * y[i,j] <= h[i,t])
-    for i in range(I):
-        for t in range(r[i]-1):
-            solver.Add(h[i,t] <= h[i,t + 1])
-    for i in range(I):
-        for t in range(s[i], T-1):
-            solver.Add(h[i,t + 1] <= h[i,t])
-    
-    for t in range(T):
-        solver.Add(solver.Sum(h[i,t] for i in range(I)) <= 17)
     sol = solver.Solve()
     if sol == pywraplp.Solver.OPTIMAL:
         print('Solution Optimal')
@@ -71,7 +50,6 @@ def model(c,c_prime, k, r, s, p, I, J, T):
             for j in range(J):
                 print('x(%d,%d) = %.2f' % (i,j,x[i,j].solution_value()) )
                 print('y(%d,%d) = %.2f' % (i,j,y[i,j].solution_value()) )
-                print('h(%d,%d) = %.2f' % (i,j,h[i,j].solution_value()) )
                 print("walltime n milisecs =", solver.WallTime())
                 print("Model time", time() - start_time, "seconds")
                 z = solver.Objective().Value()
@@ -87,7 +65,6 @@ def model(c,c_prime, k, r, s, p, I, J, T):
         for j in range(J):
             if y[i,j].solution_value() == 1.0:
                 print(f'Y -> {i}, {j}: {y[i,j].solution_value()}')
-
 def main():
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog='Dancing Wolves',
